2e2299a5a194b00be1d52e6fc6886d3c9d19d8b9
Jesse <jesse@email.com>
2025-02-07 11:58:21 +0100

    Created abstract file system component

---
+++ FileSystemCompositePattern/FileSystemCompositePattern/FileSystemComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemCompositePattern
+{
+    abstract class FileSystemComponent
+    {
+        private string _name;
+
+        protected FileSystemComponent(string name)
+        {
+               _name = name;    
+        }
+        
+        protected abstract void Display();
+        protected abstract void Add(FileSystemComponent component);
+        protected abstract void Remove(FileSystemComponent component);
+    }
+}
